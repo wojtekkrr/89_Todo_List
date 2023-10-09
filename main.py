c9e730ff80bb32f8d39c5ecc620baed4e6e29457
Wojtek2810 <75879048+Wojtek2810@users.noreply.github.com>
2023-10-09 10:51:53 +0200

    Utworzenie relacyjnych baz danych.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, url_for, redirect, request, abort, flash
 from flask_login import UserMixin, login_user, LoginManager, current_user, logout_user
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import relationship
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'Flask_Key'
@@ -22,16 +23,26 @@ db.init_app(app)
 
 # CREATE TABLE
 class Task(db.Model):
+    __tablename__ = "task"
     id = db.Column(db.Integer, primary_key=True)
+    # Create Foreign Key
+    author_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+    # Create reference to the User object. The "tasks" refers to the tasks property in the User class.
+    author = relationship("User", back_populates="tasks")
     text = db.Column(db.String(250), nullable=False)
+
 
 
 # User table for all your registered users
 class User(UserMixin, db.Model):
+    __tablename__ = "users"
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(100), unique=True, nullable=False)
     password = db.Column(db.String(100), nullable=False)
     name = db.Column(db.String(100), nullable=False)
+    # This will act like a list of Task objects attached to each User.
+    # The "author" refers to the author property in the Task class.
+    tasks = relationship("Task", back_populates="author")
 
 
 with app.app_context():
