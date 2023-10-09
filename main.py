7d4c130074601d68c2e5f64068092885a2d63955
Wojtek2810 <75879048+Wojtek2810@users.noreply.github.com>
2023-10-09 09:04:27 +0200

    Pobiera zadania z formularza i zapisuje w bazie danych.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, redirect, request, abort, flash
 from flask_login import UserMixin, login_user, LoginManager, current_user, logout_user
 from flask_sqlalchemy import SQLAlchemy
 
@@ -15,7 +15,7 @@ def load_user(user_id):
 
 
 # CREATE DB
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///cafes.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///task.db'
 db = SQLAlchemy()
 db.init_app(app)
 
@@ -38,11 +38,24 @@ with app.app_context():
     db.create_all()
 
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def home():
+    if request.method == "POST":
+        task_data = request.form
+
+        new_task = Task(
+            text=task_data["text"],
+        )
+
+        db.session.add(new_task)
+        db.session.commit()
+        return redirect(url_for("home"))
+
     result = db.session.execute(db.select(Task))
     tasks = result.scalars().all()
     return render_template("index.html", all_tasks=tasks, current_user=current_user)
+
+
 
 
 if __name__ == "__main__":
